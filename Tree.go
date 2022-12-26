0bcbc1b2ee49020cec3186aabcc2c9c497b1583e
oriys <Kirin.Toudou@protonmail.com>
2022-12-27 00:00:12 +0800

    我想我不会恨你

--- Tree.go
+++ Tree.go
@@ -118,3 +118,16 @@ func postorderTraversal(root *TreeNode) []int {
 	}
 	return append(append(postorderTraversal(root.Left), postorderTraversal(root.Right)...), root.Val)
 }
+
+func expandBinaryTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Left != nil {
+		root.Right = &TreeNode{Val: -1, Left: expandBinaryTree(root.Left)}
+	}
+	if root.Right != nil {
+		root.Right = &TreeNode{Val: -1, Right: expandBinaryTree(root.Right)}
+	}
+	return root
+}
